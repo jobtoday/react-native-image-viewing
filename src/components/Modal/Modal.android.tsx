8567eefe9b29a92c76d6962532f083fd7ed5def1
ManigandanRaamanathan <manigandanraamanathan@gmail.com>
2020-02-16 00:00:04 +0800

    Added hideStatusBar property and styling in android modal

--- src/components/Modal/Modal.android.tsx
+++ src/components/Modal/Modal.android.tsx
@@ -8,42 +8,29 @@
 
 import React, { useEffect } from "react";
 import { BackHandler, View, StyleSheet, StatusBar } from "react-native";
-
-type Props = {
-  visible: boolean;
-  transparent: boolean;
-  children: JSX.Element;
-  onRequestClose: () => void;
+const Modal = ({ visible, children, onRequestClose, hideStatusBar }) => {
+    useEffect(() => {
+        const backHandler = BackHandler.addEventListener("hardwareBackPress", () => {
+            if (typeof onRequestClose === "function") {
+                onRequestClose();
+            }
+            return true;
+        });
+        return () => {
+            backHandler.remove();
+        };
+    }, []);
+    return <>{visible && <View style={[styles.root, hideStatusBar? styles.hideStatusBar : {}]}>{children}</View>}</>;
 };
-
-const Modal = ({ visible, children, onRequestClose }: Props) => {
-  useEffect(() => {
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      () => {
-        if (typeof onRequestClose === "function") {
-          onRequestClose();
-        }
-
-        return true;
-      }
-    );
-
-    return () => {
-      backHandler.remove();
-    };
-  }, []);
-
-  return <>{visible && <View style={styles.root}>{children}</View>}</>;
-};
-
 const styles = StyleSheet.create({
-  root: {
-    ...StyleSheet.absoluteFillObject,
-    zIndex: 1000,
-    backgroundColor: "transparent",
-    top: StatusBar.currentHeight
-  }
+    root: {
+        ...StyleSheet.absoluteFillObject,
+        zIndex: 1000,
+        backgroundColor: "transparent",
+        top: StatusBar.currentHeight
+    },
+    hideStatusBar: {
+        top: 0   
+    }
 });
-
 export default Modal;
