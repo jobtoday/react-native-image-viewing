b5e95a6139838cb937e3a912a958ce2ef5df603b
Maksim Bolshakov <maksimko@gmail.com>
2020-05-17 14:15:47 +0200

    Add License

--- src/hooks/useBackHandler.ts
+++ src/hooks/useBackHandler.ts
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) JOB TODAY S.A. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
 import { useEffect, useCallback } from "react";
 import { BackHandler } from "react-native";
 
